767c9e9e1fbebf10e3e545bbef0e830ef821784b
Tan Xin Ji Alan <tan.xin.ji.alan@gmail.com>
2017-06-01 15:58:37 +0800

    Added Screen Orientation, Closing virtual keyboard and Toast popup due to invalid attempt

--- app/src/main/java/com/tanxinjialan/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/tanxinjialan/myfirstapplication/MainActivity.java
@@ -1,14 +1,13 @@
 package com.tanxinjialan.myfirstapplication;
 
-import android.support.annotation.FloatRange;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,34 +30,40 @@ public class MainActivity extends AppCompatActivity {
         compute_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                // Close virtual keyboard on button ("Compute My BMI") press
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+                // Check the length of both height_value and weight_value if more than zero
                 if (height_value.getText().length() > 0 && weight_value.getText().length() > 0) {
                     height = Float.parseFloat(height_value.getText().toString());
                     weight = Float.parseFloat(weight_value.getText().toString());
 
-                    BMI = calculateBMI(weight, height);
+                    // Check value of height and weight if in the suitable range
+                    if (height > 20.0 && weight > 0.0) {
 
-                    if (BMI < 16) result.setText("YourBMI: " + BMI + "( Severely underweight )");
-                    else if (BMI < 18.5) result.setText("YourBMI: " + BMI + "( Underweight )");
-                    else if (BMI < 25) result.setText("YourBMI: " + BMI + "( Normal )");
-                    else if (BMI < 30) result.setText("YourBMI: " + BMI + "( Overweight )");
-                    else result.setText("YourBMI: " + BMI + "( Obese )");
-                }
+                        BMI = calculateBMI(weight, height);
+
+                        if (BMI < 16)
+                            result.setText("YourBMI: " + BMI + "( Severely underweight )");
+                        else if (BMI < 18.5) result.setText("YourBMI: " + BMI + "( Underweight )");
+                        else if (BMI < 25) result.setText("YourBMI: " + BMI + "( Normal )");
+                        else if (BMI < 30) result.setText("YourBMI: " + BMI + "( Overweight )");
+                        else result.setText("YourBMI: " + BMI + "( Obese )");
+
+                    } else
+                        Toast.makeText(getApplicationContext(), "Invalid Entry", Toast.LENGTH_SHORT).show();
+                } else
+                    Toast.makeText(getApplicationContext(), "Please enter Height and/or Weight", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
+    // Own define method to do BMI calculation
     private float calculateBMI(float weight, float height) {
         height = (height / 100);
-        return (float) (weight / (height * height));
+        return (weight / (height * height));
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
-    }
 }
